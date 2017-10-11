9589bd4f73c111db4f851196e82789e58f14d047
Amr Almaz <amra0760@hstat.org>
2017-10-11 06:12:56 +0000

    Finito

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,23 +1,23 @@
-def division
+def division(num1,num2)
+    num1/num2
+end
+
+def argue(user_argument)
+    user_argument
+end
+
+def greeting(user_greeting,name)
     
-end
-
-def argue
-  
-end
-
-def greeting
-
 end
 
 def return_a_value
-    
+    "Nice"
 end
 
-def pizza_party
-    
+def pizza_party(ya_boi = "cheese")
+    ya_boi
 end
 
-def whisper
-    
+def whisper(say_something)
+    (say_something).downcase
 end
